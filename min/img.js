c36745e9513e2c171f760ad9de2826744c61bc11
Jez Allan <jez@makerlabs.co.uk>
2018-05-31 18:34:09 +0100

    Improved some terrible code with async/await

--- min/img.js
+++ min/img.js
@@ -1,51 +1,36 @@
 /*jslint node:true*/
 module.exports = function (files) {
-    'use strict';
-    var Q = require('q'),
-        log = require('../utils').log;
+  'use strict';
+  const log = require('../utils').log;
+  const imagemin = require('imagemin');
+  const imageminJpegtran = require('imagemin-jpegtran');
+  const imageminPngquant = require('imagemin-pngquant');
+  const imageminSvgo = require('imagemin-svgo');
 
-    const imagemin = require('imagemin');
-    const imageminJpegtran = require('imagemin-jpegtran');
-    const imageminPngquant = require('imagemin-pngquant');
+  const run = (file) => {
+    const originalSize = file.contents.length;
+    return imagemin.buffer(file.contents, {
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({quality: '65-80'}),
+        imageminSvgo({plugins: [{removeViewBox: false}]})
+      ]
+    }).then((compressedFile) => {
+      file.contents = compressedFile;
+      const compressedSize = compressedFile.length;
+      const compression = Math.round((compressedSize / originalSize) * 100);
+      log.info('[%s] compressed to %s% of original', file.name, compression);
+      return file;
+    }).catch((e) =>{
+      log.error('[%s] ERROR on compression, skipping', file.name);
+    });
+  }
 
+  const main = async () => {
+    if (!files.map) { files = [files]; }
+    const compressed = await files.map(run);
+    return compressed;
+  }
 
-
-    function run(file) {
-        var df = Q.defer(),
-            // imagemin = new Imagemin(),
-            originalSize = file.contents.length;
-      return imagemin.buffer(file.contents, {
-        plugins: [
-          imageminJpegtran(),
-          imageminPngquant({quality: '65-80'})
-        ]
-      
-      }).then((files) => {
-        var compressedSize = files[0].contents.length,
-          compression = 100
-          - Math.ceil((compressedSize * 100) / originalSize);
-
-        file.contents = files[0].contents;
-        if (compression) {
-          log.info('[%s] compressed in %s%', file.name, compression);
-        }
-        df.resolve(df);
-        return df.promise;
-      }).catch((e) =>{
-        if (e) {
-          log.error(e);
-          log.error('[%s] ERROR on compression, skipping', file.name);
-          return df.resolve(df);
-        }
-      });
-    }
-
-    function main() {
-        var d = Q.defer();
-        if (!files.map) { files = [files]; }
-        Q.all(files.map(run)).then(d.resolve, d.reject);
-        return d.promise;
-    }
-
-    return main();
+  return main();
 };
