17de026c30b64a523a07bfc38d3e40a842c7d434
Jez Allan <jez@makerlabs.co.uk>
2018-05-31 17:51:45 +0100

    Update imagemin attempt. Seriously wtf

--- min/img.js
+++ min/img.js
@@ -2,34 +2,42 @@
 module.exports = function (files) {
     'use strict';
     var Q = require('q'),
-        Imagemin = require('imagemin'),
         log = require('../utils').log;
+
+    const imagemin = require('imagemin');
+    const imageminJpegtran = require('imagemin-jpegtran');
+    const imageminPngquant = require('imagemin-pngquant');
+
+
 
     function run(file) {
         var df = Q.defer(),
-            imagemin = new Imagemin(),
+            // imagemin = new Imagemin(),
             originalSize = file.contents.length;
+      return imagemin.buffer(file.contents, {
+        plugins: [
+          imageminJpegtran(),
+          imageminPngquant({quality: '65-80'})
+        ]
+      
+      }).then((files) => {
+        var compressedSize = files[0].contents.length,
+          compression = 100
+          - Math.ceil((compressedSize * 100) / originalSize);
 
-        imagemin.src(file.contents).run(function (e, files) {
-            if (e) {
-                log.error(e);
-                log.error('[%s] ERROR on compression, skipping', file.name);
-                return df.resolve(df);
-                //return df.reject(e);
-            }
-
-            var compressedSize = files[0].contents.length,
-                compression = 100
-                    - Math.ceil((compressedSize * 100) / originalSize);
-
-            file.contents = files[0].contents;
-            if (compression) {
-                log.info('[%s] compressed in %s%', file.name, compression);
-            }
-            df.resolve(df);
-        });
-
+        file.contents = files[0].contents;
+        if (compression) {
+          log.info('[%s] compressed in %s%', file.name, compression);
+        }
+        df.resolve(df);
         return df.promise;
+      }).catch((e) =>{
+        if (e) {
+          log.error(e);
+          log.error('[%s] ERROR on compression, skipping', file.name);
+          return df.resolve(df);
+        }
+      });
     }
 
     function main() {
